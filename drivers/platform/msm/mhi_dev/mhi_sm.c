9b98ea472ad822abedf078d5c61643e76c004388
Bruno Martins <bgcngm@gmail.com>
2025-02-16 11:11:30 +0000

    msm: mhi_dev: Fix comparison of different enum types
    Change-Id: Ic03b412749accae07f2247b9ac67b133b7583227

--- drivers/platform/msm/mhi_dev/mhi_sm.c
+++ drivers/platform/msm/mhi_dev/mhi_sm.c
@@ -1150,7 +1150,7 @@ static void mhi_sm_pcie_event_manager(struct work_struct *work)
 			flush_workqueue(mhi->ring_init_wq);
 			mhi->stop_polling_m0 = false;
 			/* Avoid backing up mmio twice */
-			if (old_dstate != EP_PCIE_EVENT_PM_D3_HOT)
+			if (old_dstate != MHI_SM_EP_PCIE_D3_HOT_STATE)
 				mhi_dev_backup_mmio(mhi_sm_ctx->mhi_dev);
 		}
 
